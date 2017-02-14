fd6de16bbbe986d040e15b6525ea1f7138f85006
Tony Minott <minottanthonya@gmail.com>
2017-02-14 15:47:25 -0500

    SOMETHING WORKS!

---
+++ modules/fact/client/config/fact.client.config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// fact module config
+angular.module('fact').run(['Menus',
+  function (Menus) {
+		// config logic
+	Menus.addMenuItem('topbar', {
+		title:'History Facts',
+		state:'facts',
+		roles:['*']
+	});
+  }
+]);
